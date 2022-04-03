bd47105d9a64bd7fa1d982749540643661b57c21
VladWick <91905024+VladWick@users.noreply.github.com>
2022-04-03 17:32:44 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/BalanceController.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import com.example.demo.model.TransferBalance;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@RestController("/balance")
+@AllArgsConstructor
+public class BalanceController {
+
+    private final BankService bankService;
+
+    @GetMapping("/{accountId}")
+    public BigDecimal getBalance(@PathVariable Long accountId) {
+        return bankService.getBalance(accountId);
+    }
+
+
+    @PostMapping("/add")
+    public BigDecimal addMoney(@RequestBody TransferBalance transferBalance) {
+        return bankService.addMoney(transferBalance.getTo(), transferBalance.getAmount());
+    }
+
+    @PostMapping("/transfer")
+    public void transfer(@RequestBody TransferBalance transferBalance) {
+        bankService.makeTransfer(transferBalance);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String handle(IllegalArgumentException e) {
+        log.error(e.getMessage());
+        return "MAMA, YA SLOMALSYA";
+    }
+
+}
